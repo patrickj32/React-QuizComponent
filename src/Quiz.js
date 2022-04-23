f5efd630bd0af991a13f77ce1f89e534a1fb5d98
Patrick Justice <80479370+patrickj32@users.noreply.github.com>
2022-04-23 15:10:19 -0400

    created componet

---
+++ src/Quiz.js
@@ -0,0 +1,18 @@
+import React, {Componet} from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Componet{
+
+    render(){
+
+        
+        return <div>Quiz</div>
+    }
+    constructor(props) {
+        super(props)
+    }
+
+}
+
+export default Quiz
