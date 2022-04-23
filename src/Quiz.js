9d7c82fa7c4962baeb47ad6fdaa6b335b76b096a
Patrick Justice <80479370+patrickj32@users.noreply.github.com>
2022-04-23 15:55:33 -0400

    catching up

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,8 +1,8 @@
-import React, { Componet } from 'react'
+import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-class Quiz extends Componet {
+class Quiz extends Component {
     constructor(props){
         super(props)
         this.state = { quiz_position: 1}
