f6286b44d8c3a7aa4f3d84a677db41ec79382e71
Patrick Justice <80479370+patrickj32@users.noreply.github.com>
2022-04-25 07:46:48 -0400

    fixed 8

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,16 +3,19 @@ import React, { Component } from 'react'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = { quiz_position: 1}
+        this.state = { quiz_position: 1 }
     }
     render() {
         return (
-            <div className='Quizquestion'></div>
+            <div className='QuizQuestion'></div>
         )
     }
 }
 
 
 export default Quiz
+
+// Inside of that div with a className of QuizQuestion, access the first element in the 
+// quiz_questions property of the quizData object, and display the instruction_text value.
