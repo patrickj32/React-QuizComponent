4fe598a7a37885292ea303c515d232d9e996f2f2
Patrick Justice <80479370+patrickj32@users.noreply.github.com>
2022-04-25 07:49:32 -0400

    8 again

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,9 @@ class Quiz extends Component {
     }
     render() {
         return (
-            <div className='QuizQuestion'></div>
+            <div>
+                <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+            </div>
         )
     }
 }
